c3a53f825af4a1f93486f7ca4071b41feda4e461
nghia <nghia.nm@taki.vn>
2023-05-09 13:52:05 +0700

    update header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Nav from "components/nav/Nav";
 
 const App = () => {
   return (
     <>
       <Header />
+      <About />
+      <Contact />
+      <Experience />
+      <Nav />
       <Footer />
     </>
   );
--- src/components/header/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./header.css";
-
-export const Header = () => {
-  return <div>Header</div>;
-};
-
-export default Header;
